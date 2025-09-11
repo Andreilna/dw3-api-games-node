e2cd6795428813edd61d8125e3a60a4d02958810
Andreilna <andreialda7@gmail.com>
2025-09-10 22:18:38 -0300

    @Andreilna

--- 05-autenticacao-com-jsonwebtoken/middlewere/Auth.js
+++ 05-autenticacao-com-jsonwebtoken/middlewere/Auth.js
@@ -5,7 +5,23 @@ import userController from "../controllers/userController.js";
 const Authorization = (req, res, next) => {
   const authToken = req.headers["authorization"];
   if (authToken != undefined) {
-    next();
+    // Divdindo a string do token para eliminar o Bearer
+    const bearer = authToken.split(" ");
+    const token = bearer[1];
+    // Validando o token
+    jwt.verify(token, userController.jwtSecret, (error, data) => {
+      if (error) {
+        res.status(401).json({ error: "Token inválido" });
+        // TOKEN VALIDO
+      } else {
+        req.token = token;
+        req.loggedUser = {
+          id: data.id,
+          email: data.email,
+        };
+        next();
+      }
+    });
   } else {
     res.status(401).json({ error: "Token inválido ou não enviado" });
   }
